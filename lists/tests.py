7ed38faf835ed3dc10b725eba070aaa9d856a5c7
Jonathan Harrison <jharrison12@gmail.com>
2016-06-10 12:04:54 -0500

    Add a '/' to the urls in NewListTest

--- lists/tests.py
+++ lists/tests.py
@@ -63,14 +63,20 @@ class NewListTest(TestCase):
 
 
 	def test_saving_a_POST_request(self):
-		self.client.post('lists/new', data={'item_text': 'A new list item'})
+		self.client.post(
+			'/lists/new', 
+			data={'item_text': 'A new list item'}
+		)
 
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
 	def test_redirects_after_POST(self):
-		response = self.client.post('lists/new', data={'item_text': 'A new list item'})
+		response = self.client.post(
+			'/lists/new', 
+			data={'item_text': 'A new list item'}
+		)
 
 		self.assertEqual(response.status_code, 302)
 		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -12,4 +12,7 @@ def home_page(request):
 # Create your views here.
 def view_list(request):
 	items = Item.objects.all()
-	return render(request, 'list.html', {'items':items})
+	return render(request, 'list.html', {'items': items })
+
+def new_list(request):
+	pass
