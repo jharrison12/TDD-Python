440539b814c7bfae47eb03a7d0cfb896f8de04a8
Jonathan Harrison <jharrison12@gmail.com>
2016-06-24 13:28:25 -0500

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/views.py
+++ lists/views.py
@@ -2,9 +2,11 @@ from lists.models import Item, List
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from lists.forms import ItemForm
+from lists.models import Item, List
 
 def home_page(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', {'form': ItemForm()})
 
 # Create your views here.
 def view_list(request, list_id):
