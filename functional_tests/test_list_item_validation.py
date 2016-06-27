6c8b095915c70ba96f4a97ebffd95b8ee12e8fae
Jonathan Harrison <jharrison12@gmail.com>
2016-06-27 15:27:32 -0500

    Create ExistingListValidtion test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -35,13 +35,13 @@ class ItemValidationTest(FunctionalTest):
 	def test_cannot_add_duplicate_items(self):
 		#Edith goes to the home page and starts a new list
 		self.browser.get(self.server_url)
-		self.get_item_input_box().send_keys('Buy wellies\n')
+		self.get_item_input_box().send_keys('Buy Wellies\n')
 		self.check_for_row_in_list_table('1: Buy Wellies')
 
 		#She accidently tries to enter a duplicate item
-		self.get_item_input_box().send_keys('Buy wellies\n')
+		self.get_item_input_box().send_keys('Buy Wellies\n')
 
 		#She sees a helpful error message
-		self.check_for_row_in_list_table('1: Buy wellies')
+		self.check_for_row_in_list_table('1: Buy Wellies')
 		error = self.browser.find_element_by_css_selector('.has-error')
 		self.assertEqual(error.text, "you've already got this in your list")
