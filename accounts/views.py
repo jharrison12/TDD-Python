99b93811d950365b3ced86afefd4d4f1ed513ad9
Jonathan Harrison <jharrison12@gmail.com>
2016-07-05 11:39:23 -0500

    Add python test for serverside authentication.  Set up persona authentication in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
+
+
+def persona_login(request):
+	authenticate(assertion=request.POST['assertion'])
+	return HttpResponse()
 # Create your views here.
